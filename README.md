dedc6d135eb1394823ddd1c29cc50459cc8f51bd
alejandrade <alejandrodanielandrade@gmail.com>
2018-08-04 16:52:15 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# herosstory" 
+"# herosstory" 
---
+++ desktop/src/com/gigamog/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gigamog.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gigamog.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
